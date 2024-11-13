852b9933cab309d3008757703d9a428dc4f2783e
Martin Holman <me@martinholman.co.nz>
2024-11-13 15:47:47 -0800

    Add symbolicator test

---
+++ processor/symbolicator_test.go
@@ -0,0 +1,31 @@
+package symbolicatorprocessor
+
+import (
+	"context"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestSymbolicator(t *testing.T) {
+	ctx := context.Background()
+
+	fs := newFileStore("../test_assets", zaptest.NewLogger(t))
+	sym := newBasicSymbolicator(fs)
+
+	line, err := sym.symbolicate(ctx, 0, 34, "b", "basic-mapping.js")
+
+	assert.NoError(t, err)
+	assert.Equal(t, "at bar(basic-mapping-original.js:8:1)", line)
+
+	line, err = sym.symbolicate(ctx, 0, 34, "b", "does-not-exist.js")
+	assert.Error(t, err)
+
+	_, err = sym.symbolicate(ctx, math.MaxInt64, 34, "b", "basic-mapping.js")
+	assert.Error(t, err)
+
+	_, err = sym.symbolicate(ctx, 0, math.MaxInt64, "b", "basic-mapping.js")
+	assert.Error(t, err)
+}
