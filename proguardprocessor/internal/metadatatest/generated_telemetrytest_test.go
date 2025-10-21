6cdf73d0366ccf93f9bff2976574b573389145eb
Jairo <jairomendoza@honeycomb.io>
2025-10-21 13:22:08 -0700

    mdatagen changes

--- proguardprocessor/internal/metadatatest/generated_telemetrytest_test.go
+++ proguardprocessor/internal/metadatatest/generated_telemetrytest_test.go
@@ -10,7 +10,6 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
 
-	"github.com/honeycombio/opentelemetry-collector-symbolicator/proguardprocessor/internal/metadata"
 	"go.opentelemetry.io/collector/component/componenttest"
 )
 
