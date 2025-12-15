2c147d6864033f374d34aad3c20d9cc3e7fef0df
Jairo <jairomendoza@honeycomb.io>
2025-12-15 08:21:04 -0800

    bump processors' versions

--- dsymprocessor/factory.go
+++ dsymprocessor/factory.go
@@ -17,7 +17,7 @@ var (
 )
 
 const (
-	processorVersion = "0.0.9"
+	processorVersion = "0.0.10"
 )
 
 // createDefaultConfig creates the default configuration for the processor.
--- sourcemapprocessor/factory.go
+++ sourcemapprocessor/factory.go
@@ -17,7 +17,7 @@ var (
 )
 
 const (
-	processorVersion = "0.0.14"
+	processorVersion = "0.0.15"
 )
 
 // createDefaultConfig creates the default configuration for the processor.
